18e6b8bbb2487bcfc4520a5cf2ace022a001850e
songxingguo <xg.song@qq.com>
2023-01-06 22:50:08 +0800

    feat: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu>
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon>
+              <message />
+            </el-icon>导航
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <router-link to="/">基本操作</router-link>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+              <router-link to="/about">立方体</router-link>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  主页
+</template>
