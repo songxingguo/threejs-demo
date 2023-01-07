f5dfa1f07fc8e6afd053a1d6777eb4a89de60c7a
songxingguo <xg.song@qq.com>
2023-01-07 09:29:39 +0800

    feat: 新增立方体

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about"
+    ref="aboutDom">
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import init from "./gemo";
+const aboutDom = ref(null);
+onMounted(() => {
+  const { renderer } = init();
+  aboutDom.value.appendChild(renderer.domElement);
+});
+</script>
 
 <style>
 </style>
